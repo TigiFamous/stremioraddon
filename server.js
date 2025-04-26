bcb7f7fe7f5781c4828ca07d3c2f852efa64155f
TigiFamous <tigifamous@gmail.com>
2025-04-26 17:52:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -83,3 +83,9 @@ app.get("/", (req, res) => {
         </html>
     `);
 });
+const port = 3000;
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
